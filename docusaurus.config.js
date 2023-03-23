b1b91c8b932fb9d2fa5504c59dcd29c7f6cf6928
Nguyen Binh <69001423+nguyenbinhit@users.noreply.github.com>
2023-03-23 16:01:11 +0700

    doc Printcart Webhooks

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,13 +7,29 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
   module.exports = {
     title: "Printcart",
     tagline: "Web-to-print API",
+    favicon: "img/logo-printcart.png",
+
+    // Set the production url of your site here
     url: "https://your-docusaurus-test-site.com",
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: "/",
+    
     onBrokenLinks: "throw",
     onBrokenMarkdownLinks: "warn",
-    favicon: "img/logo-printcart.png",
+
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
     organizationName: "Printcart", // Usually your GitHub org/user name.
     projectName: "Printcart", // Usually your repo name.
+
+    // Even if you don't use internalization, you can use this field to set useful
+    // metadata like html lang. For example, if your site is Chinese, you may want
+    // to replace "en" with "zh-Hans".
+    i18n: {
+      defaultLocale: 'en',
+      locales: ['en'],
+    },
 
     presets: [
       [
